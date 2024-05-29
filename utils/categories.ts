23b5300da1041f9c555dc9243de68b3f5a7d0601
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2024-05-29 22:24:10 +0200

    feat(controller): endpoint login & signup added

--- utils/categories.ts
+++ utils/categories.ts
@@ -1,9 +1,6 @@
 import { Category } from "@/types";
 
 export const CATEGORIES: Category[] = [
-  { id: 1, name: "React", slug: "react" },
-  { id: 2, name: "Next.js", slug: "nextjs" },
-  { id: 3, name: "React Native", slug: "reactnative" },
-  { id: 4, name: "CSS", slug: "css" },
-  { id: 5, name: "Javascript", slug: "Javascript" },
+  { id: 1, name: "Recherche Services", slug: "recherche service" },
+  { id: 2, name: "Proposer Service", slug: "Proposer service" },
 ];
