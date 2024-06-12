f660033d798f7d38f8a2acb99f7cf30341cc226b
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2024-06-12 23:43:21 +0200

    feat(controller): posts endpoint added

---
+++ app/api/posts/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { getJWTPayLoad } from "../util/auth";
+import { sql } from "@/db";
+
+
+export async function GET(request: Request) {
+    const jwtPayload = await getJWTPayLoad();
+    const {searchParams} = new URL(request.url);
+    const username = searchParams.get("username");
+    const page = (searchParams.get("page") && parseInt(searchParams.get("page")!)) || 0;
+    const limit = 10;
+    const offset = page * 10;
+
+    const statement = `select p.*, u.avatar, u.username 
+        from posts p inner join users u 
+        on p.user_id = u.id where user_id = $1
+        order by created_at desc limit $2 offset $3`;
+
+    if(username){
+        // TODO
+        const userRes = await sql("select * from users where username = $1", [username]);
+        if(userRes.rowCount == 0) {
+           return NextResponse.json({error: "not found"}, {status: 404});  
+        } 
+        const user = userRes.rows[0];
+        const postRes = await sql(statement, [user.id, limit, offset]);
+        return NextResponse.json({data: postRes.rows});
+    }
+    const res = await sql(statement, [jwtPayload.sub, limit, offset]);
+
+    return NextResponse.json({data: res.rows});
+}
+
+
+export async function POST(request: Request) {
+    const json = await request.json();
+    const content = json.content;
+    const jwtPayload = await getJWTPayLoad();
+    const res = await sql("insert into posts (user_id, content) values ($1, $2) returning *", [jwtPayload.sub, content]);
+    return NextResponse.json({data: res.rows[0]}, {status: 201});
+}
